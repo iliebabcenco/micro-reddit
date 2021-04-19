38ca354078c6e8adbbb84507239644088217fd7d
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-04-19 16:28:59 +0300

    Add validates for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20 }
+    validates :email, presence: true, uniqueness: true, length: { minimum: 8, maximum: 20 }
 end
